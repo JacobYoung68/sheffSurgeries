243a08aa8d33b5762c358c3cef09226a7ed02ddd
Student <student@B6010137.localdomain>
2019-04-01 12:19:46 +0100

    Fixed problems with relationships and bootstrap, completed (3/7) bootstrap domain class data

--- appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -3,6 +3,26 @@ package com.jyoung
 class BootStrap {
   def init = { servletContext ->
 
+    def shc = new Surgery(
+      name:             'Sheffield Health Centre',
+      address:          '1 SHC, Main St',
+      postcode:         'S1 1AB',
+      telephone:        '0114 458 8954',
+      numberOfPatients: 410,
+      description:      'Lorem ipsum.',
+      openingTime:      '8am'
+    ).save()
+
+    def ehc = new Surgery(
+      name:             'Encliffe Health Centre',
+      address:          '2 EHC, Old Ln',
+      postcode:         'S11 2BC',
+      telephone:        '0114 458 5746',
+      numberOfPatients: 290,
+      description:      'Lorem ipsum.',
+      openingTime:      '8.30am'
+    ).save()
+
     def jSmith = new Doctor(
       fullName:      'John Smith',
       qualification: 'MBChB(Manchester)',
@@ -11,7 +31,8 @@ class BootStrap {
       password:      'verysecurepassword123',
       doctorOffice:  '11b',
       doctorPhone:   '0114 458 6548',
-      bio:           'Lorem ipsum.'
+      bio:           'Lorem ipsum.',
+      surgery:       shc
     ).save()
 
     def eDoe = new Doctor(
@@ -19,10 +40,11 @@ class BootStrap {
       qualification: 'MBChB(Sheffield)',
       position:      'GP, Surgeon',
       doctorEmail:   'eDoe_Doc@health.com',
-      password:      'password123',
-      doctorOffice:  '8a',
-      doctorPhone:   '0114 458 1548',
-      bio:           'Lorem ipsum.'
+      password:      'notsecure123',
+      doctorOffice:  '1a',
+      doctorPhone:   '0114 458 3254',
+      bio:           'Lorem ipsum.',
+      surgery:       ehc
     ).save()
 
     def aRichards = new Nurse(
@@ -30,38 +52,29 @@ class BootStrap {
       qualifications:  'Registered General Nurse',
       nurseEmail:      'aRichards_Nurse@health.com',
       nurseOffice:     '2a',
-      nursePhone:      '0114 458 1295'
-      ).save()
+      nursePhone:      '0114 458 1295',
+      surgery:         shc,
+      doctor:          jSmith
+    ).save()
 
-      def bFred = new Nurse(
-        nurseName:       'Barry Fred',
-        qualifications:  'Registered General Nurse',
-        nurseEmail:      'bFred_Nurse@health.com',
-        nurseOffice:     '2b',
-        nursePhone:      '0114 458 1872'
-        ).save()
+    def bFred = new Nurse(
+      nurseName:       'Barry Fred',
+      qualifications:  'Registered General Nurse',
+      nurseEmail:      'bFred_Nurse@health.com',
+      nurseOffice:     '4a',
+      nursePhone:      '0114 458 9548',
+      surgery:         ehc,
+      doctor:          eDoe
+    ).save()
 
-      def sheffieldHealthCentre = new Surgery(
-        name:             'Sheffield Health Centre',
-        address:          '1 SHC, Main St',
-        postcode:         'S1 1AB',
-        telephone:        '0114 458 8954',
-        numberOfPatients: '410',
-        description:      'Lorem ipsum.',
-        openingTime:      '8am'
-      ).save()
+shc.addToDoctor(jSmith)
+shc.addToNurse(aRichards)
 
-      def rotherhamHealthCentre = new Surgery(
-        name:             'Rotherham Health Centre',
-        address:          '2 RHC, Other Main St',
-        postcode:         'S16 2BC',
-        telephone:        '0114 458 5975',
-        numberOfPatients: '340',
-        description:      'Lorem ipsum.',
-        openingTime:      '8.30am'
-      ).save()
+ehc.addToDoctor(eDoe)
+ehc.addToNurse(bFred)
 
-
+jSmith.addToNurse(aRichards)
+eDoe.addToNurse(bFred)
 
   }
   def destroy = {
